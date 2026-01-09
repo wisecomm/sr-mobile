bf6db9004db2553d5c9dd146e259c699e7723dd8
danyoh <danyoh#gmail.com>
2026-01-09 16:15:37 +0900

    1

--- nextjs-client/components/data-table/data-table.tsx
+++ nextjs-client/components/data-table/data-table.tsx
@@ -74,7 +74,7 @@ export function DataTable<TData>({
                             ) : (
                                 <TableRow>
                                     <TableCell colSpan={table.getAllColumns().length} className="h-24 text-center">
-                                        No results.
+                                        조회된 데이터가 없습니다.
                                     </TableCell>
                                 </TableRow>
                             )}
