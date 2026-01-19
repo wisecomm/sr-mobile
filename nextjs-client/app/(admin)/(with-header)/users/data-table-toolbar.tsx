b17dbc1adfff30b47b33ebddf09d141addb58020
danyoh <danyoh#gmail.com>
2026-01-19 15:40:51 +0900

    <type>: <subject>
    <body>

--- nextjs-client/app/(admin)/(with-header)/users/data-table-toolbar.tsx
+++ nextjs-client/app/(admin)/(with-header)/users/data-table-toolbar.tsx
@@ -70,7 +70,7 @@ export function DataTableToolbar({
 
     return (
         <Card className="w-full">
-            <CardContent className="p-4 flex items-center justify-between gap-2 flex-wrap">
+            <CardContent className="flex items-center justify-between gap-4 flex-wrap">
                 <div className="flex flex-1 items-center gap-4 flex-wrap">
                     <div className="flex items-center gap-2">
                         <span className="text-sm font-medium whitespace-nowrap">이름</span>
