ea1e1777a197f3fc3a289104b56745feeb7677cc
danyoh <danyoh#gmail.com>
2026-01-20 13:20:36 +0900

    커스텀 데이터 그리드 삭제

--- nextjs-client/app/(admin)/(with-header)/roles/columns.tsx
+++ nextjs-client/app/(admin)/(with-header)/roles/columns.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import { ColumnDef } from "@tanstack/react-table";
 import { RoleInfo } from "./types";
-import { TextCell, UseYnCell } from "@/components/data-table/cells";
-import { createColumn, createSelectColumn } from "@/components/data-table/column-helper";
 import { SOColumnDef } from "so-grid-core";
 
 export const getColumns = (): SOColumnDef<RoleInfo>[] => [
